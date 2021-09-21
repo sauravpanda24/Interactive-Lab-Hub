412e903404008bc9901274b62ae109462afc9f14
Saurav Panda <sgp65@cornell.edu>
2021-09-20 22:29:33 -0400

    Updated screen_clock

--- Lab 2/multi_clock.py
+++ Lab 2/multi_clock.py
@@ -54,36 +54,15 @@ x = 0
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
 
 # Turn on the backlight
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
-buttonA = digitalio.DigitalInOut(board.D23)
-buttonB = digitalio.DigitalInOut(board.D24)
-buttonA.switch_to_input()
-buttonB.switch_to_input()
 image2 = Image.open('day-night.png')
 image2 = image2.convert('RGB')
 image2 = image2.resize((100, 100), Image.BICUBIC)
-num2str = {
-    0: "Tweleve",
-    1: "One",
-    2: "Two",
-    3: "Three",
-    4: "Four",
-    5: "Five",
-    6: "Six",
-    7: "Seven",
-    8: "Eight",
-    9: "Nine",
-    10: "Ten",
-    11: "Eleven",
-   12: "Tweleve"
-}
-
-
 while True:
     # Draw a black filled box to clear the image.
     #image = Image.new("RGB", (width, height))
@@ -92,37 +71,23 @@ while True:
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
     current_time_str = datetime.datetime.now().strftime("%H:%M:%S")
     utc_str = datetime.datetime.utcnow().strftime("%H:%M:%S")
-    if not  buttonA.value and not buttonB.value:
-        y = top
-        draw.text((x, y),"NYC: " + current_time_str, font=font, fill="#FFFFFF")
-        y = top + 50
-        draw.text((x,y), "UTC: " +  utc_str, font=font, fill="#0000FF")
-    elif not buttonA.value and buttonB.value:
-        y = top
-        #if int(current_time_str.split(':')[0]) > 12:
-        text_time = num2str.get(int(current_time_str.split(':')[0])%12) 
-        draw.text((x, y),f"{text_time} O' Clock", font=font, fill="#FFFFFF")
-    elif buttonA.value and not buttonB.value:
-        y = top 
-        #if int(current_time_str.split(':')[0]) > 12:
-       
-        text_time = num2str.get(int(current_time_str.split(':')[0])%12)
-        if int(current_time_str.split(':')[1]) >45:
-            text_time = num2str.get((int(current_time_str.split(':')[0]) + 1)  %12)
-            text_time = "Quater to " + text_time
-        elif int(current_time_str.split(':')[1]) > 30:
-            text_time = text_time + " Thirty"
-        elif int(current_time_str.split(':')[1]) > 15:
-            text_time = "Quater past "  + text_time
-        AM_PM = "PM" if int(current_time_str.split(':')[0]) > 12 else "AM" 
-        draw.text((x, y),f"{text_time} {AM_PM}", font=font, fill="#FFFFFF")
-    else:
-       y = top 
-       draw.text((x, y),f"Btn A for AM/PM", font=font, fill="#FFFFFF")
-       y = top + 50
-       draw.text((x, y),f"Btn B for Clock time", font=font, fill="#FF0000")
-       y = top + 100
-       draw.text((x, y),f"BOTH Button for Dual time", font=font, fill="#FF00FF")
-
-    disp.image(image, rotation)
+    y = top
+    draw.text((x, y),"NYC: " + current_time_str, font=font, fill="#FFFFFF")
+    y = top + 50
+    draw.text((x,y), "UTC: " +  utc_str, font=font, fill="#0000FF")
+    # Display image.
+    #image2 = Image.open('day-night.png')
+    #image2 = image2.convert('RGB')
+    #image2 = image2.resize((100, 100), Image.BICUBIC)
+    #image2 = np.squeeze(np.asarray(image2), axis=2)
+    #print(np.shape(np.asarray(image)))
+    time.sleep(0.4)
+    a = np.zeros((135,240,3))
+    a[35:, 140:,:] = np.asarray(image2)
+    #image = np.asarray(image) + a
+    #print(image.shape)
+    #image = Image.fromarray((image * 255).astype(np.uint8))
+    disp.image(image2, rotation)
     time.sleep(0.5)
+    draw = ImageDraw.Draw(image)
+    disp.image(image, rotation)
