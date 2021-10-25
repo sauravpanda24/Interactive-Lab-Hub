cd3c8b1b22fb58ed5caa8bf9996bfc4fb31c1348
Saurav Panda <sauravgopinathpanda@gmail.com>
2021-10-25 15:11:27 -0400

    Updated"

--- Lab 4/main.py
+++ Lab 4/main.py
@@ -4,6 +4,8 @@ import adafruit_ssd1306
 from PIL import Image, ImageDraw, ImageFont
 import qwiic
 import time
+from adafruit_seesaw import seesaw, rotaryio, digitalio
+
 
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -25,6 +27,21 @@ oled.show()
 WIDTH = 128
 HEIGHT = 32  # Change to 64 if needed
 BORDER = 5
+base_time = "00:00:00"
+
+seesaw = seesaw.Seesaw(board.I2C(), addr=0x36)
+
+seesaw_product = (seesaw.get_version() >> 16) & 0xFFFF
+print("Found product {}".format(seesaw_product))
+if seesaw_product != 4991:
+    print("Wrong firmware loaded?  Expected 4991")
+
+seesaw.pin_mode(24, seesaw.INPUT_PULLUP)
+button = digitalio.DigitalIO(seesaw, 24)
+button_held = False
+
+encoder = rotaryio.IncrementalEncoder(seesaw)
+last_position = None
 
 while True:
     image = Image.new("1", (oled.width, oled.height))
@@ -34,7 +51,7 @@ while True:
     font = ImageFont.load_default()
 
     # Draw Some Text
-    text = "Hello World!"
+    text = base_time
     (font_width, font_height) = font.getsize(text)
     draw.text(
         (oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
@@ -44,6 +61,21 @@ while True:
     )
     oled.image(image)
     oled.show()
+
+    # negate the position to make clockwise rotation positive
+    position = -encoder.position
+
+    if position != last_position:
+        last_position = position
+        print("Position: {}".format(position))
+
+    if not button.value and not button_held:
+        button_held = True
+        print("Button pressed")
+
+    if button.value and button_held:
+        button_held = False
+        print("Button released")
     try:
         ToF.start_ranging()  # Write configuration bytes to initiate measurement
         time.sleep(.005)
