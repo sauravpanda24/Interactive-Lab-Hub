b8e68e7ed4ca3a8663f44bab85f8f079501f6fac
Saurav Panda <sgp65@cornell.edu>
2021-10-25 16:53:54 -0400

    data update

---
+++ Lab 4/oled_test_1.py
@@ -0,0 +1,88 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import board
+import busio
+import adafruit_ssd1306
+from PIL import Image, ImageDraw, ImageFont
+
+
+# Create the I2C interface.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the SSD1306 OLED class.
+# The first two parameters are the pixel width and pixel height.  Change these
+# to the right size for your display!
+oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+
+
+# Helper function to draw a circle from a given position with a given radius
+# This is an implementation of the midpoint circle algorithm,
+# see https://en.wikipedia.org/wiki/Midpoint_circle_algorithm#C_example for details
+def draw_circle(xpos0, ypos0, rad, col=1):
+    x = rad - 1
+    y = 0
+    dx = 1
+    dy = 1
+    err = dx - (rad << 1)
+    while x >= y:
+        oled.pixel(xpos0 + x, ypos0 + y, col)
+        oled.pixel(xpos0 + y, ypos0 + x, col)
+        oled.pixel(xpos0 - y, ypos0 + x, col)
+        oled.pixel(xpos0 - x, ypos0 + y, col)
+        oled.pixel(xpos0 - x, ypos0 - y, col)
+        oled.pixel(xpos0 - y, ypos0 - x, col)
+        oled.pixel(xpos0 + y, ypos0 - x, col)
+        oled.pixel(xpos0 + x, ypos0 - y, col)
+        if err <= 0:
+            y += 1
+            err += dy
+            dy += 2
+        if err > 0:
+            x -= 1
+            dx += 2
+            err += dx - (rad << 1)
+
+
+# initial center of the circle
+center_x = 63
+center_y = 15
+# how fast does it move in each direction
+x_inc = 1
+y_inc = 1
+# what is the starting radius of the circle
+radius = 8
+
+# start with a blank screen
+oled.fill(0)
+# we just blanked the framebuffer. to push the framebuffer onto the display, we call show()
+oled.show()
+WIDTH = 128
+HEIGHT = 32  # Change to 64 if needed
+BORDER = 5
+while True:
+    image = Image.new("1", (oled.width, oled.height))
+    draw = ImageDraw.Draw(image)
+
+    # Draw a white background
+    draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+
+    # Draw a smaller inner rectangle
+    draw.rectangle(
+        (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
+        outline=0,
+        fill=0,
+    )
+    font = ImageFont.load_default()
+
+    # Draw Some Text
+    text = "Hello World!"
+    (font_width, font_height) = font.getsize(text)
+    draw.text(
+    (oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
+    text,
+    font=font,
+    fill=255,
+    )
+    oled.image(image)
+    oled.show()
